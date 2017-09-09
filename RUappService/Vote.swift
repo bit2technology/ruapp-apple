c1520eef54d5828aba0d46a72f1df6cc9db476e0
Igor Camilo <igor.rcamilo@gmail.com>
2017-09-09 12:49:57 -0300

    Fix for Vote

--- RUappService/Vote.swift
+++ RUappService/Vote.swift
@@ -17,16 +17,8 @@ open class Vote {
         guard let type = type else {
             throw Error.noType
         }
-        // Build dictionary
-        var dict = ["dish_id": item.id, "vote_type_id": type.rawValue] as [String : Any]
-        if let reason = reason, !reason.isEmpty {
-            dict["pre_defined_comment_ids"] = Array(reason)
-        }
-        if let comment = comment {
-            dict["comment"] = comment
-        }
         // Return dictionary
-        return dict
+        return ["dish_id": item.id, "vote_type_id": type.rawValue, "pre_defined_comment_ids": Array(reason ?? []), "comment": comment ?? ""] as [String : Any]
     }
     
     public enum VoteType: Int {
@@ -59,7 +51,12 @@ public extension Array where Element : Vote {
             let params = ["student_id": studentId, "meal_id": mealId, "votes": votesDict] as [String : Any]
             req.httpBody = params.appPrepare()
             Alamofire.request(req).responseJSON { (response) in
-                print("vote completed success:", response.result.isSuccess, response.result.value)
+                switch response.result {
+                case .success(let value):
+                    print("vote completed success:", value)
+                case .failure(let error):
+                    print("vote completed error:", error)
+                }
             }
         } catch {
             completion(.failure(error: error))
