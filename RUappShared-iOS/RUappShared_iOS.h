b67af7f83023d5af5a9668b1cafbb2bcea872a4b
Igor Camilo <igor.rcamilo@gmail.com>
2017-09-13 15:59:28 -0300

    iOS framework target added

---
+++ RUappShared-iOS/RUappShared_iOS.h
@@ -0,0 +1,19 @@
+//
+//  RUappShared_iOS.h
+//  RUappShared-iOS
+//
+//  Created by Igor Camilo on 13/09/17.
+//  Copyright © 2017 Bit2 Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RUappShared_iOS.
+FOUNDATION_EXPORT double RUappShared_iOSVersionNumber;
+
+//! Project version string for RUappShared_iOS.
+FOUNDATION_EXPORT const unsigned char RUappShared_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RUappShared_iOS/PublicHeader.h>
+
+
