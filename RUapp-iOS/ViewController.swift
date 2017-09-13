6c28be59fd99a11d43a7b05694b65a48bb11c05e
Igor Camilo <igor.rcamilo@gmail.com>
2017-09-13 15:58:27 -0300

    iOS app target added

---
+++ RUapp-iOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RUapp-iOS
+//
+//  Created by Igor Camilo on 13/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
