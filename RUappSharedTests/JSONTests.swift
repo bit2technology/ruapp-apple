d8aa1aaa05c694c391eaba4ddc3609b4ef75b3a9
Igor Camilo <igor.rcamilo@gmail.com>
2017-09-13 16:25:07 -0300

    Bundle ID update and initial test configuration

---
+++ RUappSharedTests/JSONTests.swift
@@ -0,0 +1,17 @@
+//
+//  JSONTests.swift
+//  RUappSharedTests
+//
+//  Created by Igor Camilo on 13/09/17.
+//  Copyright © 2017 Bit2 Technology. All rights reserved.
+//
+
+import XCTest
+import RUappShared
+
+class JSONTests: XCTestCase {
+    
+    func testMenu() {
+        XCTAssert(Test.test == "Test")
+    }
+}
