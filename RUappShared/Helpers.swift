0d7acc2810b640f32303740bbd07737847d098a8
Igor Camilo <igor.rcamilo@gmail.com>
2017-09-13 20:35:48 -0300

    URLRequest helper added

--- RUappShared/Helpers.swift
+++ RUappShared/Helpers.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2017 Bit2 Technology. All rights reserved.
 //
 
+public typealias CompletionHandler<T> = (() throws -> T) -> Void
+
 func sharedDirectoryURL() -> URL {
     return FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.technology.bit2.ruapp")!
 }
---
+++ RUappShared/URLRequest+Custom.swift
@@ -0,0 +1,31 @@
+//
+//  URLRequest+Custom.swift
+//  RUappShared
+//
+//  Created by Igor Camilo on 13/09/17.
+//  Copyright © 2017 Bit2 Technology. All rights reserved.
+//
+
+public extension URLRequest {
+    func response(completion: @escaping CompletionHandler<Data>) {
+        URLSession.shared.dataTask(with: self) { (data, response, error) in
+            if let error = error {
+                completion {
+                    throw error
+                }
+            } else if let data = data {
+                completion {
+                    return data
+                }
+            } else {
+                completion {
+                    throw URLRequestError.unknown
+                }
+            }
+        }.resume()
+    }
+}
+
+public enum URLRequestError: Error {
+    case unknown
+}
