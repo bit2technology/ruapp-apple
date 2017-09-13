9acfd7573e9f408d670ff5b27f4122d0cd013da5
Igor Camilo <igor.rcamilo@gmail.com>
2017-09-13 16:11:32 -0300

    Initial configuration

---
+++ RUappShared/RUappShared.h
@@ -0,0 +1,15 @@
+//
+//  RUappShared.h
+//  RUappShared
+//
+//  Created by Igor Camilo on 13/09/17.
+//  Copyright © 2017 Bit2 Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RUappShared.
+FOUNDATION_EXPORT double RUappSharedVersionNumber;
+
+//! Project version string for RUappShared.
+FOUNDATION_EXPORT const unsigned char RUappSharedVersionString[];
---
+++ RUappShared/Test.swift
@@ -0,0 +1,11 @@
+//
+//  Test.swift
+//  RUappShared
+//
+//  Created by Igor Camilo on 13/09/17.
+//  Copyright © 2017 Bit2 Technology. All rights reserved.
+//
+
+public struct Test {
+    public static let test = "Test"
+}
