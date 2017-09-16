4892b24cd6fb41a54ded5cd343630f306fed0233
Igor Camilo <igor.rcamilo@gmail.com>
2017-09-16 12:46:34 -0300

    Edit student screen reworked

--- RUapp-iOS/EditStudentController.swift
+++ RUapp-iOS/EditStudentController.swift
@@ -9,64 +9,54 @@
 import UIKit
 import RUappShared
 
-class EditStudentController: UITableViewController {
+class EditStudentController: UIViewController {
     
-    @IBOutlet private weak var nameField: UITextField!
-    @IBOutlet private weak var numberPlateField: UITextField!
-    private weak var loadingController: UIViewController?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        if let student = Student.shared {
-            nameField.text = student.name
-            numberPlateField.text = student.numberPlate
-        } else {
-            navigationItem.leftBarButtonItems = nil
-            navigationItem.rightBarButtonItem?.isEnabled = false
-            navigationItem.title = NSLocalizedString("EditStudentController.viewDidLoad.navigationItemTitle", value: "Sign Up", comment: "Title for sign up")
+    private weak var tableController: EditStudentTableController! {
+        didSet {
+            tableController.container = self
         }
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        nameField.becomeFirstResponder()
+    private var nameField: UITextField {
+        return tableController.nameField
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        switch segue.identifier {
-        case "LoadingScreen"?:
-            loadingController = segue.destination
-        default:
-            break
-        }
+    private var numberPlateField: UITextField {
+        return tableController.numberPlateField
     }
     
-    @IBAction private func cancelButtonPressed() {
-        dismiss(animated: true)
+    @IBOutlet private weak var loadingView: UIActivityIndicatorView!
+    
+    @IBAction func cancelButtonPressed() {
+        view.endEditing(true)
+        performSegue(withIdentifier: "UnwindToRoot", sender: nil)
     }
     
-    @IBAction private func doneButtonPressed() {
-        guard let name = nameField.text, name.count > 0 else {
-            nameField.becomeFirstResponder()
+    @IBAction func doneButtonPressed() {
+        guard let name = nameField.text, name.count > 0, let numberPlate = numberPlateField.text, numberPlate.count > 0 else {
+            let missingValuesAlertTitle = NSLocalizedString("EditStudentController.doneButtonPressed.missingValuesAlertTitle", value: "Missing Values", comment: "Alert title to missing values")
+            let missingValuesAlertMessage = NSLocalizedString("EditStudentController.doneButtonPressed.missingValuesAlertMessage", value: "Both name and number plate are required", comment: "Alert message to missing values")
+            let missingValuesAlertBtn = NSLocalizedString("EditStudentController.doneButtonPressed.missingValuesAlertBtn", value: "OK", comment: "Button to dismiss the alert to missing values")
+            let alert = UIAlertController(title: missingValuesAlertTitle, message: missingValuesAlertMessage, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: missingValuesAlertBtn, style: .default))
+            present(alert, animated: true)
             return
         }
-        let numberPlate = numberPlateField.text ?? ""
         view.endEditing(true)
-        performSegue(withIdentifier: "LoadingScreen", sender: nil)
+        setLoadingLayout(true)
         let process = { [weak self] (result: () throws -> Any) in
             do {
                 _ = try result()
-                DispatchQueue.main.async {
-                    self?.loadingController?.performSegue(withIdentifier: "UnwindToRoot", sender: nil)
-                }
+                self?.performSegue(withIdentifier: "UnwindToRoot", sender: nil)
             } catch {
+                DispatchQueue.main.async {
+                    self?.setLoadingLayout(false)
+                }
                 let registerErrorAlertTitle = NSLocalizedString("EditStudentController.doneButtonPressed.registerErrorAlertTitle", value: "Error", comment: "Alert title to register error")
                 let registerErrorAlertBtn = NSLocalizedString("EditStudentController.doneButtonPressed.registerErrorAlertBtn", value: "OK", comment: "Button to dismiss the alert to register error")
                 let alert = UIAlertController(title: registerErrorAlertTitle, message: error.localizedDescription, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: registerErrorAlertBtn, style: .default) { (_) in
-                    self?.loadingController?.dismiss(animated: true)
-                })
-                self?.loadingController?.present(alert, animated: true)
+                alert.addAction(UIAlertAction(title: registerErrorAlertBtn, style: .default))
+                self?.present(alert, animated: true)
             }
         }
         if let student = Student.shared {
@@ -82,20 +72,66 @@ class EditStudentController: UITableViewController {
         }
     }
     
-    @IBAction private func nameFieldEdited() {
-        navigationItem.rightBarButtonItem?.isEnabled = (nameField.text?.count ?? 0) > 0
+    private func setLoadingLayout(_ loading: Bool) {
+        navigationItem.leftBarButtonItem?.isEnabled = !loading
+        navigationItem.rightBarButtonItem?.isEnabled = !loading
+        UIView.animate(withDuration: 0.2) { [weak self] in
+            self?.loadingView.alpha = loading ? 1 : 0
+        }
     }
     
     @IBAction private func unwindToEditStudent(segue: UIStoryboardSegue) { }
 }
 
-extension EditStudentController: UITextFieldDelegate {
+extension EditStudentController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let student = Student.shared {
+            nameField.text = student.name
+            numberPlateField.text = student.numberPlate
+        } else {
+            navigationItem.leftBarButtonItems = nil
+            navigationItem.rightBarButtonItem?.isEnabled = false
+            navigationItem.title = NSLocalizedString("EditStudentController.viewDidLoad.navigationItemTitle", value: "Sign Up", comment: "Title for sign up")
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if Student.shared == nil {
+            nameField.becomeFirstResponder()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "TableController"?:
+            tableController = segue.destination as! EditStudentTableController
+        default:
+            break
+        }
+    }
+}
+
+class EditStudentTableController: UITableViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberPlateField: UITextField!
+    weak var container: EditStudentController!
+    
+    @IBAction private func nameFieldEdited() {
+        container.navigationItem.rightBarButtonItem?.isEnabled = (nameField.text?.count ?? 0) > 0
+    }
+}
+
+extension EditStudentTableController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         switch textField {
         case nameField:
             numberPlateField.becomeFirstResponder()
         case numberPlateField:
-            doneButtonPressed()
+            container.doneButtonPressed()
         default:
             break
         }
