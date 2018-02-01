0697e97d48b40edb3c0c9a1c6cae396ab3f3b04f
Igor Camilo <igor.rcamilo@gmail.com>
2018-02-01 19:19:51 -0200

    Add watchOS framework target.

---
+++ RUappShared-watchOS/RUappShared_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  RUappShared_watchOS.h
+//  RUappShared-watchOS
+//
+//  Created by Igor Camilo on 01/02/18.
+//  Copyright © 2018 Bit2 Technology. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for RUappShared_watchOS.
+FOUNDATION_EXPORT double RUappShared_watchOSVersionNumber;
+
+//! Project version string for RUappShared_watchOS.
+FOUNDATION_EXPORT const unsigned char RUappShared_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RUappShared_watchOS/PublicHeader.h>
+
+
