73c54aa40934a82545516615db131e518ab226ae
Igor Camilo <igor.rcamilo@gmail.com>
2018-02-04 18:36:27 -0200

    Initial configuration. Shared schemes.

---
+++ RUappShared/Test.swift
@@ -0,0 +1,11 @@
+//
+//  Test.swift
+//  RUappShared
+//
+//  Created by Igor Camilo on 01/02/18.
+//  Copyright © 2018 Bit2 Technology. All rights reserved.
+//
+
+class Test {
+    let test = "Test from RUappShared"
+}
