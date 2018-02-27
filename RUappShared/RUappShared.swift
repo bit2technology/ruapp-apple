7220242d8a8af18b859e5995c2973eb0d72618fb
Igor Camilo <igor.rcamilo@gmail.com>
2018-02-27 16:04:35 -0300

    Add iOS framework target.

---
+++ RUappShared/RUappShared.swift
@@ -0,0 +1,11 @@
+//
+//  RUappShared.swift
+//  RUappShared
+//
+//  Created by Igor Camilo on 27/02/18.
+//  Copyright © 2018 Bit2 Technology. All rights reserved.
+//
+
+class RUappShared {
+  static let test = "Hello, RUappShared!"
+}
