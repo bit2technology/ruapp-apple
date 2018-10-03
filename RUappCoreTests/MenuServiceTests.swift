3f59cc578bbae6a190efe830dd25592f84ab0f63
Igor Camilo <igor.rcamilo@gmail.com>
2018-10-03 07:05:38 -0300

    More tests

--- RUappCoreTests/MenuServiceTests.swift
+++ RUappCoreTests/MenuServiceTests.swift
@@ -4,13 +4,40 @@ import CoreData
 
 class MenuServiceTests: XCTestCase {
 
-    func testUpdate() {
+    func testSuccessfulUpdate() {
         let exp = expectation(description: "Update menu task")
-        let url = Bundle(for: MenuServiceTests.self).url(forResource: "menu", withExtension: "json")!
-        MenuService.updateMenu(request: URLRequest(url: url), context: PersistentContainer.test.newBackgroundContextForUpdate()) { (error) in
+        let url = Bundle(for: MenuServiceTests.self).url(forResource: "Menu", withExtension: "json")!
+        let context = PersistentContainer.forTesting().newBackgroundContextForUpdate()
+        MenuService.updateMenu(request: URLRequest(url: url), context: context) { (error) in
             XCTAssertNil(error)
             let req: NSFetchRequest<Meal> = Meal.fetchRequest()
-            XCTAssertEqual(try! PersistentContainer.test.viewContext.count(for: req), 17)
+            XCTAssertEqual(try! context.count(for: req), 17)
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 10)
+    }
+    
+    func testUnsuccessfulUpdate() {
+        let exp = expectation(description: "Update menu task")
+        let url = Bundle(for: MenuServiceTests.self).url(forResource: "MenuWithError", withExtension: "json")!
+        let context = PersistentContainer.forTesting().newBackgroundContextForUpdate()
+        MenuService.updateMenu(request: URLRequest(url: url), context: context) { (error) in
+            XCTAssertNotNil(error)
+            let req: NSFetchRequest<Meal> = Meal.fetchRequest()
+            XCTAssertEqual(try! context.count(for: req), 0)
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 10)
+    }
+    
+    func testNotFoundUpdate() {
+        let exp = expectation(description: "Update menu task")
+        let url = Bundle(for: MenuServiceTests.self).bundleURL.appendingPathComponent("NotFound.json", isDirectory: false)
+        let context = PersistentContainer.forTesting().newBackgroundContextForUpdate()
+        MenuService.updateMenu(request: URLRequest(url: url), context: context) { (error) in
+            XCTAssertNotNil(error)
+            let req: NSFetchRequest<Meal> = Meal.fetchRequest()
+            XCTAssertEqual(try! context.count(for: req), 0)
             exp.fulfill()
         }
         waitForExpectations(timeout: 10)
--- RUappCoreTests/PersistentContainer.swift
+++ RUappCoreTests/PersistentContainer.swift
@@ -2,7 +2,7 @@ import RUappCore
 import CoreData
 
 extension PersistentContainer {
-    static let test: PersistentContainer = {
+    static func forTesting() -> PersistentContainer {
         let modelURL = Bundle(for: PersistentContainer.self).url(forResource: "Model", withExtension: "momd")!
         let container = PersistentContainer(name: "Model", managedObjectModel: NSManagedObjectModel(contentsOf: modelURL)!)
         let storeDescription = NSPersistentStoreDescription()
@@ -15,5 +15,5 @@ extension PersistentContainer {
         }
         container.viewContext.automaticallyMergesChangesFromParent = true
         return container
-    }()
+    }
 }
