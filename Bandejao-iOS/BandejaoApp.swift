790e5852606680446975661a00683bd04fe15e67
Igor Camilo <igor.rcamilo@gmail.com>
2021-04-13 18:11:27 +0200

    Add iOS target.

---
+++ Bandejao-iOS/BandejaoApp.swift
@@ -0,0 +1,18 @@
+//
+//  BandejaoApp.swift
+//  Bandejao-iOS
+//
+//  Created by Igor Camilo on 13.04.21.
+//  Copyright © 2021 Firebrick. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BandejaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bandejao-iOS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Bandejao-iOS
+//
+//  Created by Igor Camilo on 13.04.21.
+//  Copyright © 2021 Firebrick. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
